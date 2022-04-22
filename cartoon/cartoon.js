5eaa0955a736d6e9616951c43caab8db36009d6f
junyiee <jennycheng816@gmail.com>
2022-04-22 02:11:31 -0400

    updated cartoon
    added sun, clouds, and background; enlarged canvas size

--- cartoon/cartoon.js
+++ cartoon/cartoon.js
@@ -1,58 +1,77 @@
 function draw() {
-    var canvas = document.getElementById('canvas');
-    if (canvas.getContext) {
-      var ctx = canvas.getContext('2d');
+  var canvas = document.getElementById('canvas');
+  if (canvas.getContext) {
+    var ctx = canvas.getContext('2d');
+    ctx.fillStyle = "	#F0FFFF";
+    ctx.fillRect(0, 0, canvas.width, canvas.height);
   
-      // flip the coordinate frame
-      ctx.save();
-      flipY(ctx); 
+    ctx.save(); 
       
-      // calling functions to populate village
-      drawTreeAt(ctx,200,50,90,20,30);
-      drawTreeAt(ctx,100,60,90,20,30);
-      drawTreeAt(ctx,50,25,90,20,30);
-      drawTreeAt(ctx,340,65,90,20,30);
-      drawTreeAt(ctx,365,25,90,20,30);
-      drawTreeAt(ctx,265,115,90,20,30);
-      drawHouseAt(ctx,200,50,30,40,10,6);
-      drawHouseAt(ctx,90,30,30,40,10,6);
+    // calling functions to populate village
+    drawTreeAt(ctx,700,350,90,20,30);
+    drawTreeAt(ctx,600,360,90,20,30);
+    drawTreeAt(ctx,550,325,90,20,30);
+    drawTreeAt(ctx,840,365,90,20,30);
+    drawTreeAt(ctx,765,415,90,20,30);
+    drawHouseAt(ctx,800,500);
+    drawHouseAt(ctx,620,450);
+    drawSunAt(ctx,670,150,70);
+    drawCloudAt(ctx,100,135);
+    drawCloudAt(ctx,800,150);
+    drawCloudAt(ctx,1000,120);
+    drawCloudAt(ctx,300,165);
+    drawCloudAt(ctx,1230,200);
   
-      ctx.restore();
-    }
+    ctx.restore();
   }
+}
   
-  function drawTreeAt(ctx,x,y,height,width,radius) {
-    ctx.beginPath();
-    ctx.arc(x-25, y+150, radius, 0, Math.PI * 2, true);
-    ctx.fillStyle = "green";
-    ctx.fill();
-    
-    ctx.fillStyle = "brown";
-    ctx.fillRect(x-35, y+30, width, height);
-  }
+function drawTreeAt(ctx,x,y,height,width,radius) {
+  ctx.beginPath();
+  // draws top of tree
+  ctx.arc(x-25, y, radius, 0, Math.PI * 2, true);
+  ctx.fillStyle = "green";
+  ctx.fill();
+  // draws tree trunk
+  ctx.fillStyle = "brown";
+  ctx.fillRect(x-35, y+30, width, height);
+}
   
-  function drawHouseAt(ctx,x,y,width,height,dx,dy) {
-    ctx.beginPath();
-    ctx.moveTo(x, y);
-    ctx.lineTo(x, y+50);
-    ctx.lineTo(x+25, y+75)
-    ctx.lineTo(x+50, y+50);
-    ctx.lineTo(x+50, y);
-    ctx.lineTo(x, y);
-    ctx.moveTo(x+25, y+75);
-    ctx.lineTo(x+40, y+85);
-    ctx.lineTo(x+65, y+60);
-    ctx.lineTo(x+65, y+10);
-    ctx.lineTo(x+50, y);
-    ctx.stroke();
-  }
-  
-  // flipY moves the origin and flips the Y axis, so the coordinate system
-  // is more familiar, with the origin (0,0) in the lower left corner
-  function flipY(ctx) {
-    var canvas = ctx.canvas;
-    var w = canvas.clientWidth;
-    var h = canvas.clientHeight;
-    ctx.translate(0,h);
-    ctx.scale(1,-1);
-  }
+function drawHouseAt(ctx,x,y) {
+  ctx.beginPath();
+  // draws front structure
+  ctx.moveTo(x, y);
+  ctx.lineTo(x-25, y+35);
+  ctx.lineTo(x-25, y+85);
+  ctx.lineTo(x+25, y+85);
+  ctx.lineTo(x+25, y+35);
+  ctx.lineTo(x, y);
+  // draws side structure
+  ctx.moveTo(x, y);
+  ctx.lineTo(x+15, y-10);
+  ctx.lineTo(x+40, y+25);
+  ctx.lineTo(x+40, y+70);
+  ctx.lineTo(x+25, y+85);
+  ctx.stroke();
+}
+
+function drawSunAt(ctx,x,y,radius) {
+  ctx.beginPath();
+  ctx.arc(x, y, radius, 0, Math.PI * 2, true);
+  ctx.fillStyle = "#f9d71c";
+  ctx.fill();
+}
+
+function drawCloudAt(ctx,x,y) {
+  ctx.beginPath();
+  ctx.arc(x, y, 40, Math.PI * 0.5, Math.PI * 1.5);
+  ctx.arc(x+50, y-40, 50, Math.PI * 1, Math.PI * 1.85);
+  ctx.arc(x+132, y-25, 40, Math.PI * 0.5, Math.PI * 1.91);
+  ctx.arc(x+175, y+5, 40, Math.PI * 1.5, Math.PI * 0.35);
+  ctx.moveTo(x+175, y+40);
+  ctx.lineTo(x, y+40);
+  ctx.strokeStyle = '#797874';
+  ctx.stroke();
+  ctx.fillStyle = '#8ED6FF';
+  ctx.fill()
+}
